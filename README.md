8eb26720a740c1d3ebfdc2b4d662faca96f82e55
ndkator <39215594+ndkator@users.noreply.github.com>
2019-12-14 13:37:39 +0300

    yo!
    Math power!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Circle Waves
+
+Recreated [this cool circle wave gif animation](http://beesandbombs.tumblr.com/post/45513650541/orbiters) in HTML and Javascript. And you can play around with a few parameters to get different wave forms!
+
+This is the original gif animation:
+
+![](http://37.media.tumblr.com/352c06f02c2203e085213921a1579c3e/tumblr_mjrmkzlAPI1r2geqjo1_500.gif)
+
+And here's the [demo](http://diegosalazar.github.io/CircleWaves).
+
+Feel free to poke around.
---
+++ circle-waves.js
@@ -0,0 +1,100 @@
+var play = true,
+    numCircles = 500,
+    startTime = undefined,
+    speed = 3,
+    offset = 21,
+    separation = 20;
+
+$(function() {
+  var $pool = $('#pool'),
+      origCircle = $pool.find('.circle'),
+      startBtn = $('button#start'),
+      numCirclesInput = $('#numCircles'),
+
+      speedSlider = $('#speedSlider'),
+      offsetSlider = $('#offsetSlider'),
+      separationSlider = $('#separationSlider'),
+
+      speedDisplay = $('#speedDisplay'),
+      offsetDisplay = $('#offsetDisplay'),
+      separationDisplay = $('#separationDisplay');
+
+  numCircles = numCirclesInput.val();
+
+  speedSlider.slider({ value: speed });
+  offsetSlider.slider({ value: offset });
+  separationSlider.slider({ value: separation });
+
+  speedDisplay.text(speed);
+  offsetDisplay.text(offset);
+  separationDisplay.text(separation);
+
+  startBtn.click(function() {
+    var btn = $(this);
+
+    if (this.innerText == 'Stop') {
+      btn.removeClass('btn-danger').addClass('btn-success');
+      this.innerText = 'Start';
+      play = false;
+    } else {
+      btn.removeClass('btn-success').addClass('btn-danger');
+      this.innerText = 'Stop';
+      play = true;
+    }
+  });
+
+  speedSlider.on('slide', function() {
+    speed = $(this).slider('value');
+    speedDisplay.text(speed);
+  });
+
+  offsetSlider.on('slide', function() {
+    offset = $(this).slider('value');
+    offsetDisplay.text(offset);
+  });
+
+  separationSlider.on('slide', function() {
+    separation = $(this).slider('value');
+    separationDisplay.text(separation);
+
+    $('.circle').css({
+      'margin-top': '-'+ separation +'px',
+      'margin-left': '-'+ separation +'px'
+    });
+  });
+
+  numCirclesInput.on('keyup', function() {
+    numCircles = parseInt(this.value);
+    $pool.empty();
+    makePool($pool, origCircle);
+  });
+
+  makePool($pool, origCircle);
+  animatePool($pool, 0);
+});
+
+function makePool(pool, circle) {
+  for (var i = 0; i < numCircles; i++) {
+    pool.append(circle.clone());
+  }
+}
+
+function animatePool(pool, curTime) {
+  animate(pool, curTime);
+
+  requestAnimationFrame(function(time){
+    animatePool(pool, time);
+  });
+}
+
+function animate(pool, curTime) {
+  if (!play) return;
+  if (curTime === undefined) { curTime = Date.now(); }
+  if (startTime === undefined) { startTime = curTime; }
+
+  var degree = (curTime - startTime) / speed % 360;
+
+  pool.children().each(function(i, circle) {
+    $(circle).css('transform', 'rotate('+ (i * offset + degree) +'deg)')
+  });
+}
